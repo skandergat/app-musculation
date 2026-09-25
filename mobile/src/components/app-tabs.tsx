b4208eedc91281024d33cc3c5d030edd58edc42e
skandergat <br5hrdzjs4@privaterelay.appleid.com>
2026-09-25 22:50:37 +0200

    Remove Explore tab from app menu

--- mobile/src/components/app-tabs.tsx
+++ mobile/src/components/app-tabs.tsx
@@ -37,14 +37,6 @@ export default function AppTabs() {
         />
       </NativeTabs.Trigger>
 
-      <NativeTabs.Trigger name="explore">
-        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
-        />
-      </NativeTabs.Trigger>
-
       <NativeTabs.Trigger name="compte">
         <NativeTabs.Trigger.Label>Compte</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon
